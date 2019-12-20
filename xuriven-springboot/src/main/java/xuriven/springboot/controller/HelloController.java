389415ecc782bf2174a009fa08fbe1117dd67aec
XuRiven <XuRiven_Jay@163.com>
2019-12-20 15:05:58 +0800

    去掉springbootApplication注解

--- xuriven-springboot/src/main/java/xuriven/springboot/controller/HelloController.java
+++ xuriven-springboot/src/main/java/xuriven/springboot/controller/HelloController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.sql.DataSource;
 
-@SpringBootApplication
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
