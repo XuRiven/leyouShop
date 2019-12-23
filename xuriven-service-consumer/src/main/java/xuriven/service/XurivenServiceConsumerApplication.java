62a7137c1f4aa258848ba4d70570e0549ad8885c
XuRiven <XuRiven_Jay@163.com>
2019-12-23 23:29:44 +0800

    SpringCloud之Ribbon、Hystrix

--- xuriven-service-consumer/src/main/java/xuriven/service/XurivenServiceConsumerApplication.java
+++ xuriven-service-consumer/src/main/java/xuriven/service/XurivenServiceConsumerApplication.java
@@ -2,20 +2,31 @@ package xuriven.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootApplication
+/*@SpringBootApplication
 @EnableDiscoveryClient
+@EnableCircuitBreaker//开启熔断*/
+@SpringCloudApplication//组合注解,相当于SpringBootApplication + EnableDiscoveryClient + EnableCircuitBreaker
 public class XurivenServiceConsumerApplication {
 
     @Bean
-    public RestTemplate restTemplate(){
+    @LoadBalanced//开启负载均衡Ribbon
+    public RestTemplate restTemplate() {
         return new RestTemplate();
     }
 
     public static void main(String[] args) {
+        /*try {
+            Thread.sleep(20000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }*/
         SpringApplication.run(XurivenServiceConsumerApplication.class, args);
     }
 
--- xuriven-service-consumer/src/main/java/xuriven/service/controller/UserController.java
+++ xuriven-service-consumer/src/main/java/xuriven/service/controller/UserController.java
@@ -1,9 +1,12 @@
 package xuriven.service.controller;
 
 import com.netflix.appinfo.InstanceInfo;
+import com.netflix.hystrix.contrib.javanica.annotation.DefaultProperties;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,17 +19,37 @@ import java.util.List;
 
 @RequestMapping("/consumer")
 @Controller
+@DefaultProperties(defaultFallback = "fallBackMethod") //在类上指明统一的失败降级方法
 public class UserController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @Autowired
-    private DiscoveryClient discoveryClient;
+    //@Autowired
+//    private DiscoveryClient discoveryClient;
     @GetMapping("user")
     @ResponseBody
-    public User queryUserById(@RequestParam("id") Long id) {
-        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
-        ServiceInstance serviceInstance = instances.get(0);
-        return this.restTemplate.getForObject("http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id, User.class);
+    @HystrixCommand//标记该方法需要熔断
+    public String queryUserById(@RequestParam("id") Long id) {
+//        List<ServiceInstance> instances = discoveryClient.getInstances("service-provider");
+//        ServiceInstance serviceInstance = instances.get(0);
+        String baseUrl = "http://service-provider/user/" + id;
+        return this.restTemplate.getForObject(baseUrl, String.class);
     }
+
+    /**
+     * 熔断方法
+     * 返回值要和被熔断的方法返回值一致
+     * 熔断方法不需要参数
+     * 熔断:
+     *     1、closed:关闭状态，所有请求都正常访问
+     *     2、open:打开状态，所有请求都会被降级。Hystix会对请求情况计数，当一定时间内失败请求百分比达到阈值，则触发熔断，断路器会完全打开。
+     *             默认失败比例的阈值是50%，请求次数最少不低于20次。
+     *     3、Half Open：半开状态，open状态不是永久的，打开后会进入休眠时间（默认是5S）。随后断路器会自动进入半开状态。
+     *                   此时会释放部分请求通过，若这些请求都是健康的，则会完全关闭断路器，否则继续保持打开，再次进行休眠计时
+     * @return
+     */
+    public String fallBackMethod() {
+        return "服务器正忙，请稍后再试！";
+    }
+
 }
